b0a8e874212b88576e2e2c2c4a63332c29568526
yamamoto-yuta <automatuX78@gmail.com>
2020-12-14 12:20:27 +0900

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dir_history
+Fzf-Directory-History extension for [Fuzzy-Explorer-on-Terminal](https://github.com/Fuzzy-Explorer/Fuzzy-Explorer-on-Terminal)
+
+## Installation
+1. Move `dir_history/` to `~/.fet/plugins`
+1. Set keybind by editing `~/.fet/condfig-zsh`/`~/.fet/config-bash`
+
---
+++ dir_history/zsh/dir_history.zsh
@@ -0,0 +1,6 @@
+target_dir=$(cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf +m --ansi --height 70% --cycle --info='inline' --layout=reverse --border --prompt="Dir History >" --preview="echo {} | cut -f 2 -d ' ' | xargs -rI{a} sh -c 'if [ -f \"{a}\" ]; then ls -ldhG {a}; if [ \"$(which batcat)\" = \"batcat not found\" ]; then cat {a}; else batcat {a} --color=always --style=grid --line-range :100; fi else ls -ldhG {a}; echo; lsi {a}; fi'")
+target_dir=`echo ${target_dir/\~/$HOME}`
+if [ -n "$target_dir" ]; then
+    _fet_path_selected_path=$target_dir
+    . ~/.fet/function/cd.zsh
+fi
