81bdcdacef133ad2e634a9cb92708158761d3890
yamamoto-yuta <automatuX78@gmail.com>
2020-12-16 12:49:52 +0900

    👍 Add shebang

--- dir_history/bash/dir_history.bash
+++ dir_history/bash/dir_history.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 target_dir=$(cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf +m --ansi --height 70% --cycle --info='inline' --layout=reverse --border --prompt="Dir History >" --preview="echo {} | cut -f 2 -d ' ' | xargs -rI{a} sh -c 'if [ -f \"{a}\" ]; then ls -ldhG {a}; if [ \"$(which batcat)\" = \"batcat not found\" ]; then cat {a}; else batcat {a} --color=always --style=grid --line-range :100; fi else ls -ldhG {a}; echo; lsi {a}; fi'")
 target_dir=`echo ${target_dir/\~/$HOME}`
 if [ -n "$target_dir" ]; then
